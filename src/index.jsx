f3b999d6cd7e883b19a4a9c3bbdbd3cd755d7fb4
trevodev <24248975-trevodev@users.noreply.replit.com>
2023-11-30 13:17:16 +0000

    j

--- src/index.jsx
+++ src/index.jsx
@@ -1,14 +1,15 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import Header from './componentes/header/header'
-import Footer from './componentes/footer/footer'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import Header from './componentes/header/header';
+import Footer from './componentes/footer/footer';
+import Home from './pages/home.jsx'
 import './App.css'
 
 function App() {
   return (
     <div className='geral'>
       <Header />
-
+      <Home />
       <Footer />
     </div>
   )
---
+++ src/pages/home.jsx
@@ -0,0 +1,124 @@
+import React, { useState } from 'react';
+import './Home.css';
+
+function Home() {
+  const [activeTab, setActiveTab] = useState('chats'); // Estado para controlar a aba ativa
+
+  const changeTab = (tab) => {
+    setActiveTab(tab);
+  };
+
+  return (
+    <div className="home">
+      <div className="tabs">
+        <div className={`tab ${activeTab === 'chats' ? 'active' : ''}`} onClick={() => changeTab('chats')}>
+          Novidades
+        </div>
+        <div className={`tab ${activeTab === 'status' ? 'active' : ''}`} onClick={() => changeTab('status')}>
+          Utilidades
+        </div>
+        <div className={`tab ${activeTab === 'calls' ? 'active' : ''}`} onClick={() => changeTab('calls')}>
+          Chat
+        </div>
+      </div>
+      <div className="containe">
+        {/* Aqui você colocaria o conteúdo de cada aba */}
+        {activeTab === 'chats' && (
+          <>
+            <div className='box'>
+              <iframe
+                src="https://www.youtube.com/embed/qugKR-CrllI"
+                title="YouTube video player"
+                frameborder="0"
+                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                allowfullscreen
+              ></iframe>
+            </div>
+            <div className='box'>
+              <iframe
+                src="https://www.youtube.com/embed/qugKR-CrllI"
+                title="YouTube video player"
+                frameborder="0"
+                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                allowfullscreen
+              ></iframe>
+            </div>
+            <div className='box'>
+              <iframe
+                src="https://www.youtube.com/embed/qugKR-CrllI"
+                title="YouTube video player"
+                frameborder="0"
+                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                allowfullscreen
+              ></iframe>
+            </div>
+          </>
+        )}
+        {activeTab === 'status' && (
+          <>
+            <div className='box'>
+              <iframe
+                src="https://www.youtube.com/embed/dQAKRGdoSQA?si=DYn93hFGeJc9ZGLR"
+                title="YouTube video player"
+                frameborder="0"
+                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                allowfullscreen
+              ></iframe>
+            </div>
+            <div className='box'>
+              <iframe
+                src="https://www.youtube.com/embed/dQAKRGdoSQA?si=DYn93hFGeJc9ZGLR"
+                title="YouTube video player"
+                frameborder="0"
+                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                allowfullscreen
+              ></iframe>
+            </div>
+            <div className='box'>
+              <iframe
+                src="https://www.youtube.com/embed/dQAKRGdoSQA?si=DYn93hFGeJc9ZGLR"
+                title="YouTube video player"
+                frameborder="0"
+                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                allowfullscreen
+              ></iframe>
+            </div>
+          </>
+        )}
+        {activeTab === 'calls' && (
+          <>
+            <div className='box'>
+              <iframe
+                src="https://www.youtube.com/embed/dQAKRGdoSQA?si=DYn93hFGeJc9ZGLR"
+                title="YouTube video player"
+                frameborder="0"
+                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                allowfullscreen
+              ></iframe>
+            </div>
+            <div className='box'>
+              <iframe
+                src="https://www.youtube.com/embed/dQAKRGdoSQA?si=DYn93hFGeJc9ZGLR"
+                title="YouTube video player"
+                frameborder="0"
+                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                allowfullscreen
+              ></iframe>
+            </div>
+            <div className='box'>
+              <iframe
+                src="https://www.youtube.com/embed/dQAKRGdoSQA?si=DYn93hFGeJc9ZGLR"
+                title="YouTube video player"
+                frameborder="0"
+                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                allowfullscreen
+              ></iframe>
+            </div>
+          </>
+        )}
+      </div>
+    </div>
+  );
+}
+
+export default Home;

fd45a7ee68666b0e45070e317bf5a6686f030bba
Ramonsantossss <103340294+Ramonsantossss@users.noreply.github.com>
2023-12-01 13:57:16 +0000

    J

--- src/pages/mangas.jsx
+++ src/pages/mangas.jsx
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from 'react';
-import axios from 'axios';
 import './mangas.css';
 
 function Mangas() {
@@ -13,12 +12,7 @@ function Mangas() {
           throw new Error('Erro ao buscar os dados da API');
         }
         const resultado = await response.json();
-
-        if (Array.isArray(resultado)) {
-          setMangasPopular(resultado);
-        } else {
-          console.error('A resposta da API não é uma array:', resultado);
-        }
+        setMangasPopular(resultado.slice(0, 8));
       } catch (error) {
         console.error(error);
       }
@@ -29,34 +23,32 @@ function Mangas() {
 
   return (
     <div className='mangas-container'>
-      <center>
-        {mangasPopular.map((manga, index) => (
-          <div key={index} className='manga-item'>
-            <a href={manga.link} target='_blank' rel='noopener noreferrer'>
-              <div className='manga-image'>
-                <img className='capa' src={manga.imageUrl} alt={manga.title} />
-                <div className='titulo-over'>
-                  <p className='titulo'>{manga.title}</p>
-                </div>
+      {mangasPopular.map((manga, index) => (
+        <div key={index} className='manga-item'>
+          <a href={manga.link} target='_blank' rel='noopener noreferrer'>
+            <div className='manga-image'>
+              <img className='capa' src={manga.imageUrl} alt={manga.title} />
+              <div className='titulo-over'>
+                <p className='titulo'>{manga.title}</p>
               </div>
-            </a>
-            <div className='manga-info'>
-              <p className='manga-type'>{manga.mangaType}</p>
-              <p className='manga-rating'>Classificação: {manga.rating}</p>
-              <ul className='chapter-list'>
-                {manga.chapters.map((chapter, idx) => (
-                  <li key={idx}>
-                    <a href={chapter.chapterLink} target='_blank' rel='noopener noreferrer'>
-                      {chapter.chapterTitle}
-                    </a>
-                    <span className='published'>Publicado: {chapter.publishedAgo}</span>
-                  </li>
-                ))}
-              </ul>
             </div>
+          </a>
+          <div className='manga-info'>
+            <p className='manga-type'>{manga.mangaType}</p>
+            <p className='manga-rating'>Classificação: {manga.rating}</p>
+            <ul className='chapter-list'>
+              {manga.chapters.map((chapter, idx) => (
+                <li key={idx}>
+                  <a href={chapter.chapterLink} target='_blank' rel='noopener noreferrer'>
+                    {chapter.chapterTitle}
+                  </a>
+                  <span className='published'>Publicado: {chapter.publishedAgo}</span>
+                </li>
+              ))}
+            </ul>
           </div>
-        ))}
-      </center>
+        </div>
+      ))}
     </div>
   );
 }

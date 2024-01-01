f3085fb07b50b8f2c830bc4490e10136774326dc
Ramonsantossss <103340294+Ramonsantossss@users.noreply.github.com>
2024-01-01 16:57:58 +0000

    Manha

--- src/pages/caps.jsx
+++ src/pages/caps.jsx
@@ -1,18 +1,56 @@
 import React, { useState, useEffect } from 'react';
-//import './manga.scss';
-//import DisqusEmbed from './DisqusEmbed';
-import { Link } from "react-router-dom";
 
-function Caps(props){
-  const { match, location } = props;
+function Caps(props) {
+  const { match } = props;
   const { nome } = match.params;
+  const [infoAnime, setAnimeInfo] = useState(null);
+  const [error, setError] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
+
+  async function fetchAnimeInfo() {
+    try {
+      const response = await fetch(`https://fine-gold-squid-yoke.cyclic.app/infomanga?nome=${nome}`);
+
+      if (!response.ok) {
+        throw new Error('Erro ao buscar os dados da API');
+      }
+
+      const resultado = await response.json();
+      setAnimeInfo(resultado);
+      setIsLoading(false);
+    } catch (error) {
+      console.error(error);
+      setError(error);
+      setIsLoading(false);
+    }
+  }
+
+  useEffect(() => {
+    fetchAnimeInfo();
+  }, [nome]);
 
   return (
-    <>
-    <h1>{nome}</h1>
-    </>
-  )
-
+    <div className="container">
+      {isLoading ? (
+        <p className="loading">Carregando...</p>
+      ) : error ? (
+        <p className="error">Erro ao buscar os dados da API.</p>
+      ) : (
+        infoAnime && (
+          <>
+            <div className="rela">
+              <img className="image" src={infoAnime.imagemCapa} alt="Capa do Livro" />
+              <div className="info">
+                <h1 className="title">{infoAnime.titulo}</h1>
+              </div>
+            </div>
+                <p className="description">{infoAnime.descricao}</p>
+              
+          </>
+        )
+      )}
+    </div>
+  );
 }
 
 export default Caps;
